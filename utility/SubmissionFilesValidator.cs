a0228f8b31f2499c4963c8278b294ffc678c5ed7
Sahil-Mane-345 <sahil.mane@zeuslearning.com>
2026-06-30 15:47:28 +0530

    File Signature Validation

--- utility/SubmissionFilesValidator.cs
+++ utility/SubmissionFilesValidator.cs
@@ -1,3 +1,4 @@
+using FileSignatures;
 using TraineeApi.Utility.Exception;
 
 namespace TraineeApi.Utility;
@@ -8,11 +9,13 @@ public static class SubmissionFilesValidator
     {
         int maxFileSize = 10 * 1024 * 1024;
 
-        List<string> allowedExtensions = [".pdf", ".jpg", ".png"];
+        HashSet<string> allowedContentTypes = ["application/pdf","image/jpeg","image/png"];
+
+        HashSet<string> allowedExtensions = [".pdf", ".jpg", ".png"];
 
         foreach(var file in formFiles)
         {
-            if( file == null)
+            if( file == null || file.Length == 0)
             {
                 throw new InvalidFileValidationException("File should not be null");
             }
@@ -24,7 +27,24 @@ public static class SubmissionFilesValidator
 
             if(!allowedExtensions.Contains(Path.GetExtension(file.FileName)))
             {
-                throw new InvalidFileValidationException( $"No file should be other than allowed extensions {string.Join(", ", allowedExtensions)}");
+                throw new InvalidFileValidationException( $"File extension {Path.GetExtension(file.FileName)} not allowed");
+            }
+
+            if (!allowedContentTypes.Contains(file.ContentType))
+            {
+                throw new InvalidFileValidationException($"File Content-type {file.ContentType} not allowed");
+            }
+
+            using var stream = file.OpenReadStream();
+            var inspector = new FileFormatInspector();
+
+            var format = inspector.DetermineFileFormat(stream) ?? throw new InvalidFileValidationException("Unable to determine type");
+            
+            string detectedExtension = "." + format.Extension.ToLowerInvariant();
+
+            if(!string.Equals(detectedExtension, Path.GetExtension(file.FileName), StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidFileValidationException("File signature does not match the file extension");
             }
         }
             
